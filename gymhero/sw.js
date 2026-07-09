2a86283e3960b038424352b3f51fe00a81c5401b
ludovicdouy-create <ludovicdouy@gmail.com>
2026-07-09 21:07:10 +0200

    Add files via upload

--- gymhero/sw.js
+++ gymhero/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gymhero-v5';
+const CACHE = 'gymhero-v6';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
